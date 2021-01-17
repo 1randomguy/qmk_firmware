6228fda5fe364a9391682644cbefd3d47e569511
Bene <bblomberg123@gmail.com>
2021-01-17 11:17:15 +0100

    bugfixes

--- keyboards/zlant/keymaps/1randomguy/keymap.c
+++ keyboards/zlant/keymaps/1randomguy/keymap.c
@@ -4,7 +4,8 @@
 #define DVORAK 1
 #define UPPER  2
 #define LOWER  3
-
+#define UPPERD  4
+#define LOWERD  5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -19,13 +20,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_MPLY,    KC_MPRV,    KC_MNXT, KC_VOLD,   KC_VOLU, _______, RALT(KC_Y), _______, RALT(KC_P), _______,   KC_DEL,  \
     _______, RALT(KC_Q), RALT(KC_S), _______, _______,   KC_MUTE, KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT,   _______,   _______, \
     _______, _______,    _______,    _______, _______,   _______, _______, _______,   _______,  _______,    _______,   _______, \
-    _______, _______,    _______,    _______, _______,   _______, _______, RESET,     _______,  _______,    _______,   _______  \
+    _______, _______,    _______,    _______, _______,   _______, _______, _______,     RESET,  _______,    _______,   _______  \
   ),
 
   [LOWER]   = LAYOUT(
     KC_GRV , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    _______, _______, _______, KC_MINUS, KC_EQUAL,\
-    KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    _______, _______, KC_LBRC, KC_RBRC, _______, \
+    KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    _______, _______, _______, KC_MINUS, KC_EQUAL,\
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    _______, _______, KC_LBRC, KC_RBRC, _______, \
     _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______  \
   ),
 
@@ -33,7 +34,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,         \
     KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINUS,        \
     KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    RSFT_T(KC_ENTER),\
-    KC_LCTRL,DF(1), KC_LGUI, KC_LALT, MO(3),   KC_SPC,KC_SPC,    MO(2),   KC_RALT, KC_PGUP, KC_PGDOWN, KC_RCTRL         \
+    KC_LCTRL,DF(0), KC_LGUI, KC_LALT, MO(5),   KC_SPC,KC_SPC,    MO(4),   KC_RALT, KC_PGUP, KC_PGDOWN, KC_RCTRL         \
+  ),
+
+  [UPPERD]   = LAYOUT(
+    _______, KC_MPLY,    KC_MPRV,    KC_MNXT, KC_VOLD,    KC_VOLU, _______, _______, _______, _______, KC_RIGHT,   KC_DEL,  \
+    _______, RALT(KC_Q), RALT(KC_P), _______, RALT(KC_Y), KC_MUTE, _______, KC_LEFT, _______, _______, RALT(KC_S), _______, \
+    _______, _______,    _______,    KC_DOWN, KC_UP,      _______, _______, _______, _______, _______, _______,    _______, \
+    _______, _______,    _______,    _______, _______,    _______, _______, _______, _______, _______, _______,    _______  \
+  ),
+
+  [LOWERD]   = LAYOUT(
+    KC_GRV , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_SLSH, KC_EQUAL,\
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_LBRC, KC_RBRC, _______, \
+    _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______  \
   ),
 };
 
